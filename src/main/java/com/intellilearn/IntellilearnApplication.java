c3a855f4328a9d6df941a42f6b4363e2787a7859
deepak481 <deepakfeb25@gmail.com>
2026-07-06 14:44:16 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/intellilearn/IntellilearnApplication.java
@@ -0,0 +1,13 @@
+package com.intellilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellilearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellilearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intellilearn/IntellilearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellilearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
